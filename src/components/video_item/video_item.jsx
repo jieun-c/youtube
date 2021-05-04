6a8afb13c48c1515243a32d90afb5da114971dec
Development <flffl1231@naver.com>
2021-05-04 10:52:32 +0900

    make video item

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import styles from './video_item.module.css';
 
 class VideoItem extends Component {
     render() {
+        const {snippet} = this.props.video;
         return (
-            <li>
-                <h5>{this.props.video.title}</h5>
+            <li className={styles.container}>
+                <div className={styles.video}>
+                    <img className={styles.thumnails} src={snippet.thumbnails.medium.url} alt={snippet.channelTitle} />
+                    <div className={styles.metadata}>
+                        <p className={styles.title}>{snippet.title}</p>
+                        <p className={styles.channel}>{snippet.channelTitle}</p>
+                    </div>
+                </div>
             </li>
         );
     }
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import VideoItem from '../video_item/video_item';
+import styles from './video_list.module.css';
 
 class VideoList extends Component {
     render() {
         return (
-            <ul>
+            <ul className={styles.videos}>
                 {this.props.videos.map((video) => (
-                    <VideoItem key = {video.id} video = {video.snippet} />
+                    <VideoItem key = {video.id} video = {video} />
                 ))}
             </ul>
         );
