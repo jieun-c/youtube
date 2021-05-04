54d33d67863ab2beda918296c7f71e526d315217
Development <flffl1231@naver.com>
2021-05-04 17:56:45 +0900

    add Header

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './app.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 class App extends Component {
@@ -21,7 +22,10 @@ class App extends Component {
 
   render() {
     return (
-      <VideoList videos={this.state.videos}/>
+      <>
+        <SearchHeader />
+        <VideoList videos={this.state.videos}/>
+      </>
     );
   }
 }
---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import styles from './search_header.module.css';
+
+class SearchHeader extends Component {
+    inputRef = React.createRef();
+
+    handleSearch(event){
+        event.preventDefault();
+        const name = this.inputRef.current.value;
+        console.log(name);
+        this.inputRef.current.value = "";
+    }
+
+    onClick= (event) =>{
+        this.handleSearch(event);
+    }
+
+    onKeyPress = (event) =>{
+        if(event.key === 'Enter'){
+            this.handleSearch(event);
+        }
+    }
+
+    render() {
+        return (
+            <header className={styles.header}>
+                <div className={styles.logo}>
+                    <img className={styles.img} src="/images/logo.png" alt="logo"/>
+                    <h1 className={styles.title}>Youtube</h1>
+                </div>
+                <input className={styles.input} type="text" placeholder="Search.." ref={this.inputRef} onKeyPress={this.onKeyPress} />
+                <button className={styles.button} type="submit" onClick={this.onClick} >
+                    <img className={styles.buttonImg} src="/images/search.png" alt="search" />
+                </button>
+            </header>
+        );
+    }
+}
+
+export default SearchHeader;
